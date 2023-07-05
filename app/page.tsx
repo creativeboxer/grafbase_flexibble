8001a1aaf4cc8c3fcbac8a84ae706f83ea1dc75e
Unni Krishnan <unniemirate@gmail.com>
2023-07-05 23:38:30 +0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Catagories</h1>
+      <h1> Posts</h1>
+      <h1>Posts LoadMore</h1>
+    </section>
+  );
+};
+
+export default Home;
